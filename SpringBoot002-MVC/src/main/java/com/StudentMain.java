ed159df79097143d9d961a468590cf8641b55539
Administrator <Administrator@SC-201711102054>
2018-02-24 00:38:03 +0800

    微调

--- SpringBoot002-MVC/src/main/java/com/StudentMain.java
+++ SpringBoot002-MVC/src/main/java/com/StudentMain.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan({"com.control"})
+@ComponentScan("com.control")
 public class StudentMain {
     
     public static void main(String[] args) {
