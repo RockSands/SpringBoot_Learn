f16176a0edfaa236bf0e274c69298a7542772573
RockSands <117019049@qq.com>
2018-02-26 18:02:10 +0800

    增加注释

--- SpringBoot004-JPA/src/main/java/com/EmployeeMain.java
+++ SpringBoot004-JPA/src/main/java/com/EmployeeMain.java
@@ -36,10 +36,13 @@ public class EmployeeMain {
 	employee.setLastName("kuiwu");
 	employee.setHireDate(new Date());
 	employee.setGender("M");
+	// 新增
 	employeeRepository.saveAndFlush(employee);
 	System.out.println("=ADD=>SUCCESS!");
 	employee = employeeRepository.findOne(1001L);
+	// 查询一个
 	System.out.println("=QUERYONE=>" + employee.getFirstName() + employee.getLastName());
+	// 条件查询
 	Specification<Employee> conditions = new Specification<Employee>() {
 	    @Override
 	    public Predicate toPredicate(Root<Employee> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
@@ -47,13 +50,14 @@ public class EmployeeMain {
 		// query对象构建where条件语句
 		// CriteriaBuilder构建工具
 		// 一下逻辑是查询Employee表,且Where条件为gender = 'M'
-		Predicate gem = cb.equal(root.get("gender"), "M");
-		Predicate flK = cb.like(root.get("firstName"), "K%");
-		return cb.and(gem, flK);
+		Predicate and0 = cb.equal(root.get("gender"), "M");
+		Predicate and1 = cb.like(root.get("firstName"), "K%");
+		return cb.and(and0, and1);
 	    }
 	};
 	List<Employee> employees = employeeRepository.findAll(conditions);
 	System.out.println("=QUERYALL=>" + employees.size());
+	// 删除
 	employeeRepository.delete(1001L);
 	System.out.println("=DEL=>SUCCESS!");
     }
