d62d8b97d770be4f2e0a2519098719a2f6b5f90a
Administrator <Administrator@SC-201711102054>
2018-03-03 00:30:37 +0800

    事物

--- SpringBoot003-JDBC/src/main/java/com/dao/EmployeeDao.java
+++ SpringBoot003-JDBC/src/main/java/com/dao/EmployeeDao.java
@@ -2,6 +2,8 @@ package com.dao;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.domain.Employee;
 
 public interface EmployeeDao {
--- SpringBoot003-JDBC/src/main/java/com/service/EmployeeService.java
+++ SpringBoot003-JDBC/src/main/java/com/service/EmployeeService.java
@@ -2,59 +2,71 @@ package com.service;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dao.EmployeeDao;
 import com.domain.Employee;
 
+/**
+ * @Transactional 支持事物
+ *
+ */
 @Service
 public class EmployeeService implements EmployeeDao {
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-	/**
-	 * Employee的对象Mapper
-	 */
-	private static final RowMapper<Employee> rowMapper = new RowMapper<Employee>() {
-		@Override
-		public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
-			Employee employee = new Employee();
-			employee.setEmpNo(rs.getLong("emp_no"));
-			employee.setBirthDate(rs.getDate("birth_date"));
-			employee.setFirstName(rs.getString("first_name"));
-			employee.setLastName(rs.getString("last_name"));
-			employee.setGender(rs.getString("gender"));
-			employee.setHireDate(rs.getDate("hire_date"));
-			return employee;
-		}
-	};
-
+    /**
+     * Employee的对象Mapper
+     */
+    private static final RowMapper<Employee> rowMapper = new RowMapper<Employee>() {
 	@Override
-	public void add(Employee employee) {
-		jdbcTemplate.update(
-				"INSERT INTO employees (emp_no, birth_date, first_name, last_name, gender, hire_date) VALUES (?, ?, ?, ?, ?, ?)",
-				employee.getEmpNo(), employee.getBirthDate(), employee.getFirstName(), employee.getLastName(),
-				employee.getGender(), employee.getHireDate());
+	public Employee mapRow(ResultSet rs, int rowNum) throws SQLException {
+	    Employee employee = new Employee();
+	    employee.setEmpNo(rs.getLong("emp_no"));
+	    employee.setBirthDate(rs.getDate("birth_date"));
+	    employee.setFirstName(rs.getString("first_name"));
+	    employee.setLastName(rs.getString("last_name"));
+	    employee.setGender(rs.getString("gender"));
+	    employee.setHireDate(rs.getDate("hire_date"));
+	    return employee;
 	}
+    };
 
-	@Override
-	public void delete(long empNo) {
-		jdbcTemplate.update("DELETE FROM employees WHERE emp_no= ?", empNo);
-	}
+    @Override
+    @Transactional
+    public void add(Employee employee) {
+	jdbcTemplate.update(
+		"INSERT INTO employees (emp_no, birth_date, first_name, last_name, gender, hire_date) VALUES (?, ?, ?, ?, ?, ?)",
+		employee.getEmpNo(), employee.getBirthDate(), employee.getFirstName(), employee.getLastName(),
+		employee.getGender(), employee.getHireDate());
+	jdbcTemplate.update("UPDATE employees SET hire_date = ? WHERE emp_no= ?", new Date(System.currentTimeMillis() - 3600000L * 24L),
+		employee.getEmpNo());
+    }
 
-	@Override
-	public List<Employee> queryAll() {
-		return jdbcTemplate.query("SELECT * FROM employees WHERE gender = 'M' AND first_name LIKE 'K%'", rowMapper);
-	}
+    @Override
+    @Transactional
+    public void delete(long empNo) {
+	jdbcTemplate.update("DELETE FROM employees WHERE emp_no= ?", empNo);
+    }
 
-	@Override
-	public Employee queryByNo(long empNo) {
-		return jdbcTemplate.queryForObject("SELECT * FROM employees WHERE emp_no = ?", new Object[] { empNo },
-				rowMapper);
-	}
+    @Override
+    @Transactional(readOnly = true)
+    public List<Employee> queryAll() {
+	return jdbcTemplate.query("SELECT * FROM employees WHERE gender = 'M' AND first_name LIKE 'K%'", rowMapper);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Employee queryByNo(long empNo) {
+	return jdbcTemplate.queryForObject("SELECT * FROM employees WHERE emp_no = ?", new Object[] { empNo },
+		rowMapper);
+    }
 }
