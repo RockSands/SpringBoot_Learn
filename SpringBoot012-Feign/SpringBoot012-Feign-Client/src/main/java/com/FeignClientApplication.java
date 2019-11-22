708ff9215314133253b35bdcc477d2c663a51176
chenkuiwu <Administrator@SC-201910191835>
2019-11-22 10:32:05 +0800

    修改为OpenFeign

--- SpringBoot012-Feign/SpringBoot012-Feign-Client/src/main/java/com/FeignClientApplication.java
+++ SpringBoot012-Feign/SpringBoot012-Feign-Client/src/main/java/com/FeignClientApplication.java
@@ -2,7 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.api.MyClient;
@@ -10,13 +10,12 @@ import com.api.MyClient;
 @SpringBootApplication
 @EnableFeignClients
 public class FeignClientApplication {
-	
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(FeignClientApplication.class, args);
 		MyClient client = context.getBean(MyClient.class);
-		for(int i=0;i<10;i++) {
+		for (int i = 0; i < 10; i++) {
 			System.out.println("--->" + client.method1("33333"));
 		}
-		//System.out.println("--->" + client.get1(1001L, "ckw"));
 	}
 }
